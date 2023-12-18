4b326b7dc1aab9f106f745aa7370ebb8c47a68b9
rishi <rishi18december@gmail.com>
2023-12-18 13:56:09 +0530

    simplifying folder

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ def get_blogs():
     return [{"blog" : "Ye le nikaal photu |( ^  ^ )|"}]
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__=="__main__":
+#     app.run(host="0.0.0.0", port=5000)
 
 
 
